635de8f1c5e4abe455e4d8b73efae4774d6d5c2a
programmer9129 <programmer9129@gmail.com>
2026-09-01 02:19:17 +0530

    Update console-app-1mark.cpp

---
+++ console-app-1mark.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+bool basher(string a)
+{
+	if (a.find("--valcon ", 0) == 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+int main()
+{
+	std::string line;
+	std::getline(std::cin, line);
+	if (basher(line) == true)
+	{
+		std::string command = line.substr(9);
+		std::cout << "Command: " << command << std::endl;
+	}
+}
