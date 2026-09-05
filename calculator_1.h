8cfc1ca624976210886971d56836ae46a8d17db6
programmer9129 <programmer9129@gmail.com>
2026-09-05 16:12:35 +0530

    Update calculator_1.h

--- calculator_1.h
+++ calculator_1.h
@@ -4,6 +4,7 @@
 #include<string>
 #include<vector>
 #include<algorithm>
+#include<cmath>
 using namespace std;
 
 class Calculator
@@ -20,12 +21,12 @@ public:
 		std::vector<int> numbers;
 		std::vector<int>sums_unification;
 		std::vector<char> operations;
-		bool TOKEN = false;
+		bool TOKEN = false;int j = 0;
 		int count = 0;int count_mark = 0;
 		line.erase(remove(line.begin(), line.end(), '='), line.end());
 		for (int i = 0;i < line.length();i++)
 		{
-			for (int j = 0;j < 4;j++)
+			for (j = 0;j < 4;j++)
 			{
 				if (line[i] == operation[j])
 				{
@@ -40,21 +41,29 @@ public:
 			}
 			if (TOKEN == false)
 			{
-				count = +1;
-				numbers.push_back(line[i]);
+				count += 1;
+				int num_standby = int(line[i]) - '0';
+				numbers.push_back(num_standby);
 			}
 			else
 			{
 				reverse(numbers.begin(), numbers.end());
 				for (int n = numbers.size() - 1;n >= 0;n--)
 				{
-					sums_unified = sums_unified + numbers[n] * (10 ^ n);
+					sums_unified = sums_unified + (numbers[n] * pow(10,n));
 				}
 				sums_unification.push_back(sums_unified);
-				operations.push_back(line[i]);
 				numbers.clear();//number got intialised to 0
 				count = 0;//counter is intialized to 0
 			}
+		}
+		for (int x : sums_unification)
+		{
+			for (j = 0;j < operations.size();j++)
+			{
+
+			}
+
 		}
 
 
